8f62010ad48245f0a0a5a5e5b28cd2f33ca5d230
Fraz <frazer.irving@mailonline.co.uk>
2014-02-04 16:02:06 +0000

    jkh

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-kibit "0.0.9-DM"
+(defproject lein-kibit "0.0.10"
   :description "kibit lein plugin"
   :url "https://github.com/jonase/lein-kibit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jonase/kibit "0.0.9-DM"]]
+  :dependencies [[jonase/kibit "0.0.10"]]
   :eval-in-leiningen true)
